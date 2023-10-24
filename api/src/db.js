7964f36b571de6d9f0729a98b601a9ac43097acd
Agostina Gavilan <agosgavilan3@gmail.com>
2023-10-23 22:58:41 -0300

    modelos

--- api/src/db.js
+++ api/src/db.js
@@ -30,12 +30,12 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Dogs, Temperament } = sequelize.models;
+const { Dogs, Temperaments } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
-Dogs.belongsToMany(Temperament, {through: 'DogsxTemperament'});
-Temperament.belongsToMany(Dogs, {through: 'DogsxTemperament'})
+Dogs.belongsToMany(Temperaments, {through: 'DogsxTemperament'});
+Temperaments.belongsToMany(Dogs, {through: 'DogsxTemperament'})
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -4,9 +4,41 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('dog', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4, //me genera automaticamente un UUISV4
+      unique: true,
+      primaryKey: true,
+      allowNull: false //allowNull(permite un vacio) --> FALSO
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+      unique: true
     },
+    heightMin: {
+        type: DataTypes.DECIMAL,
+        allowNull: false
+    },
+    heightMax: {
+        type: DataTypes.DECIMAL,
+        allowNull: false
+    },
+    weightMin: {
+        type: DataTypes.DECIMAL,
+        allowNull: false
+    },
+    weightMax: {
+        type: DataTypes.DECIMAL,
+        allowNull: false
+    },
+    life_span: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    }, 
+    image: {
+        type: DataTypes.STRING,
+        allowNull: true
+    }
   });
 };
